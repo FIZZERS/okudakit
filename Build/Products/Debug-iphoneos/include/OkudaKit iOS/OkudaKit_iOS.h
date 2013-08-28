df6a542e986b964370d7db0f48f8303209497dcf
Aleksandr Kozlovskij <Aleksandr@Kozlovskij.ru>
2013-08-29 03:31:05 +0400

    try build

---
+++ Build/Products/Debug-iphoneos/include/OkudaKit iOS/OkudaKit_iOS.h
@@ -0,0 +1,13 @@
+//
+//  OkudaKit_iOS.h
+//  OkudaKit iOS
+//
+//  Created by Atsushi Nagase on 1/10/13.
+//  Copyright (c) 2013 LittleApps Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OkudaKit_iOS : NSObject
+
+@end
